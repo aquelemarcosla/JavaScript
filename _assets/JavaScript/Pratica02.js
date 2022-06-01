f1f5aa77972e833b9c318066d521cd9916fe4abe
aquelemarcosla <marcosaurelio060204@gmail.com>
2022-06-01 17:47:40 -0300

    Pratica02

--- _assets/JavaScript/Pratica02.js
+++ _assets/JavaScript/Pratica02.js
@@ -1,17 +1,39 @@
-function calculaIdade(anos) {
-    return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade +  anos} anos de idade.`;
+const pessoas = [
+    {
+        nome: 'Marcos',
+        idade: 18,
+    },
+    {
+        nome: 'Maria',
+        idade: 12,
+    },
+]
+
+const filmes = [
+    {
+        nome: 'Cavaleiro de Metal',
+        indicacao: 16,
+    },
+    {
+        nome: 'As nuvens falam',
+        indicacao: 10,
+    },
+    {
+        nome: 'Horror no pantano',
+        indicacao: 18,
+    },
+]
+
+function verificacaoIdade(filmes, pessoa) {
     
+    const {idade} = pessoa;
+    const {indicacao} = filmes;
+
+    if(idade < indicacao) {
+        return `Você tem ${idade} anos e não tem idade para ver esse filme. Poderá ver daqui ${indicacao - idade} anos`;
+    } else {
+        return `Você tem ${idade} anos e pode ver este filme.`;
+    }
 }
 
-const pessoa1 = {
-    nome: 'Marcos',
-    idade: 18,
-}
-
-const pessoa2 = {
-    nome: 'Maria',
-    idade: 78,
-}
-
-console.log(calculaIdade.call(pessoa1, 15));
-console.log(calculaIdade.apply(pessoa2, [2]));
+console.log(verificacaoIdade(filmes, pessoas));
